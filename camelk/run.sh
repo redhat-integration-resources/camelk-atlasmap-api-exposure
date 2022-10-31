82b28b5f11f100f4b725a76cf164b0d27fc4c0f9
Robert Baumgartner <rbaumgar@redhat.com>
2022-10-31 11:25:40 +0100

    update for Camel-K-Integration 1.8

--- camelk/run.sh
+++ camelk/run.sh
@@ -1,5 +1,6 @@
 kamel run --name api-layer api-route.xml api-spec.xml \
---open-api api/openapi.json \
+--dependency camel-language \
+--open-api file:api/openapi.json \
 --resource file:api/openapi.json \
 --property file:cfg/svc.properties \
 --resource file:map/request.adm \
